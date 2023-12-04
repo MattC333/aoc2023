5994af892dd1308ece0953debb0feac28f481b45
Matt Christopher <145553703+MattC333@users.noreply.github.com>
2023-12-04 19:03:08 +0000

    Day 4 part 2

--- day4/main.py
+++ day4/main.py
@@ -5,6 +5,8 @@ _ROOT = Path(__file__).resolve().parents[0]
 
 def execute(filename: str):
     running_total = 0
+    copies = {}
+    row = 1
     with open(Path(_ROOT, "data", filename), "r") as f:
         for card in f.read().splitlines():
             card_split = card.split(":")[1].strip().split("|")
@@ -14,7 +16,17 @@ def execute(filename: str):
             matches = [match for match in matches if match]
             if len(matches) > 0:
                 running_total += 2 ** (len(matches) - 1)
-    return running_total
+                instances = copies[row + 1] + 1 if row + 1 in copies.keys() else 1
+                for a in range(1, len(matches) + 1):
+                    copies[row + 1 + a] = (
+                        copies[row + 1 + a] + instances
+                        if row + 1 + a in copies.keys()
+                        else instances
+                    )
+            row += 1
+
+    part2 = (row - 1) + sum(copies.values())
+    return running_total, part2
 
 
 if __name__ == "__main__":
--- day4/tests/test_main.py
+++ day4/tests/test_main.py
@@ -2,4 +2,4 @@ from day4.main import execute
 
 
 def test_main():
-    assert execute("test_input.txt") == 13
+    assert execute("test_input.txt") == (13, 30)
